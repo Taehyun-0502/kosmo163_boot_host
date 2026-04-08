89458e83b98a9bb7a0d5a36307dadf97c86100c5
wogur6594-dotcom <wogur6594@gmail.com>
2026-04-08 18:09:32 +0900

    0408 1809

--- src/main/java/com/jh/app/professor/ProfessorController.java
+++ src/main/java/com/jh/app/professor/ProfessorController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +15,10 @@ import com.jh.app.professor.ProfessorDTO;
 @Controller
 @RequestMapping("/professor/*")
 public class ProfessorController {
-	
+
 	@Autowired
 	private ProfessorService professorService;
-	
+
 	@GetMapping("list")
 	// 리퀘스트 대신 인터페이스 모델 모델=jsp로 운반 해주는 객체, 생명주기는 리퀘스트와 같다
 	public ModelAndView list(ModelAndView mv) throws Exception {
@@ -27,6 +29,34 @@ public class ProfessorController {
 		mv.setViewName("professor/list");
 
 		return mv;
+	}
+
+	@GetMapping("detail")
+	public void detail(ProfessorDTO professorDTO, Model model) throws Exception {
+		professorDTO = professorService.detail(professorDTO);
+		model.addAttribute("d", professorDTO);
+
+	}
+
+	@GetMapping("create")
+	public void create() {
+
+	}
+
+	@PostMapping("create")
+	public String create(ProfessorDTO professorDTO) throws Exception {
+
+		int result = professorService.create(professorDTO);
+
+		return "redirect:./list";
+	}
+
+	public String delete(ProfessorDTO professorDTO) throws Exception {
+
+		int ressult = professorService.delete(professorDTO);
+
+		return "redirect:./list";
+
 	}
 
 }
